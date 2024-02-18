89682f732d6935ee63ce10101ad387c75210beb9
seagullK <91627128+seagullK@users.noreply.github.com>
2024-02-18 19:03:39 +0300

    Create TicTacToeGame.java
    Кре́стики-но́лики[1] — логическая игра между двумя противниками на квадратном поле 3 на 3 клетки или бо́льшего размера (вплоть до «бесконечного поля»). Один из игроков играет «крестиками», второй — «ноликами». В традиционной китайской игре Гомоку используются чёрные и белые камни. Игроки по очереди ставят на свободные клетки поля 3×3 знаки (один всегда крестики, другой всегда нолики). Первый, выстроивший в ряд 3 своих фигуры по вертикали, горизонтали или большой диагонали, выигрывает. Если игроки заполнили все 9 ячеек и оказалось, что ни в одной вертикали, горизонтали или большой диагонали нет трёх одинаковых знаков, партия считается закончившейся в ничью. Первый ход делает игрок, ставящий крестики. Обычно по завершении партии выигравшая сторона зачёркивает чертой свои три знака (нолика или крестика), составляющих сплошной ряд.

---
+++ TicTacToeGame.java
@@ -0,0 +1,163 @@
+/*
+Крестики - нолики
+*/
+
+package com.javarush.games.ticktacktoe;
+import com.javarush.engine.cell.*;
+
+public class TicTacToeGame extends Game {
+    
+    private int[][] model = new int[3][3];
+    private int currentPlayer;
+    private boolean isGameStopped;
+    
+    public void initialize() {
+        setScreenSize(3, 3);
+        startGame();
+        updateView();
+    }
+    
+    public void startGame() {
+        for (int i = 0; i < model.length; i++) {
+            for (int j = 0; j < model[i].length; j++) {
+                model[i][j] = 0;
+            }
+        }       
+        currentPlayer = 1;
+        isGameStopped = false;
+        
+    }
+    
+    public boolean checkFutureWin( int x, int y, int n) {
+        if (model[x][y] != 0)
+            return false;
+        model[x][y] = n;
+        boolean isFutureWin = checkWin(x, y, n);
+        model[x][y] = 0;
+        return isFutureWin;
+    }
+    
+    public void computerTurn() {
+        if (model[1][1] == 0)
+            setSignAndCheck(1, 1);
+        else {
+            for (int i = 0; i < model.length; i++) {
+                for (int j = 0; j < model[i].length; j++) {
+                    if (checkFutureWin(i, j, 2)){
+                        setSignAndCheck(i, j);
+                        return;
+                    }
+                        
+                }
+            }
+            for (int i = 0; i < model.length; i++) {
+                for (int j = 0; j < model[i].length; j++) {
+                    if (checkFutureWin(i, j, 1)){
+                        setSignAndCheck(i, j);
+                        return;
+                    }
+                        
+                }
+            }
+            for (int i = 0; i < model.length; i++) {
+                for (int j = 0; j < model[i].length; j++) {
+                    if (model[i][j] == 0){
+                        setSignAndCheck(i, j);
+                        return;
+                    } 
+                }
+            }
+        }  
+    }
+    
+    public void onKeyPress(Key key) { 
+        if (key == Key.ESCAPE || (isGameStopped && key == Key.SPACE)) {
+            startGame();
+            updateView();
+        }
+    }
+    
+    public boolean checkWin(int x, int y, int n) {
+        if (model[x][y] != n) 
+            return false;
+        if (model[x][0] == model[x][1] && model[x][0] == model[x][2])
+            return true;
+        if (model[0][y] == model[1][y] && model[0][y] == model[2][y])
+            return true;
+        if (model[0][0] == n && model[1][1] == n && model[2][2] == n )
+            return true;
+        if (model[0][2] == n && model[1][1]  == n && model[2][0] == n)
+            return true;
+        return false;
+    }
+    
+    public boolean hasEmptyCell() {
+        for (int i = 0; i < model.length; i++) {
+            for (int j = 0; j < model[i].length; j++) {
+                if (model[i][j] == 0)
+                    return true;
+            }
+        }
+        return false;
+    }
+    
+    public void onMouseLeftClick(int x, int y) {
+        
+        setSignAndCheck(x, y);
+        
+        // if (currentPlayer == 1)
+        //     currentPlayer = 2;
+        // else 
+        //     currentPlayer = 1;
+        
+        currentPlayer = 2;
+        computerTurn();
+        currentPlayer = 1;
+    }
+    
+    public void setSignAndCheck(int x, int y) {
+        
+        if (model[x][y] != 0 || isGameStopped) 
+            return;
+        model[x][y] = currentPlayer;
+        updateView();
+        if (checkWin(x, y, currentPlayer)) {
+            isGameStopped = true;
+            if (currentPlayer == 1)
+                showMessageDialog(Color.NONE, "You Win!", Color.GREEN, 75);
+            else
+                showMessageDialog(Color.NONE, "Game Over", Color.RED, 75);
+            
+            return;
+        }
+        else if (!hasEmptyCell()) {
+            isGameStopped = true;
+            showMessageDialog(Color.NONE, " Draw!",  Color.BLUE, 75);
+            return;
+        }
+    }
+    
+    public void updateView() {
+        for (int i = 0; i < model.length; i++) {
+            for (int j = 0; j < model[i].length; j++) {
+                updateCellView(i, j, model[i][j]);
+            }
+        }
+    }
+    
+    public void updateCellView(int x, int y, int value) {
+        String text = " ";
+        Color cellColor = Color.WHITE;
+        Color textColor = Color.WHITE;
+        if (value == 1) {
+            text = "X";
+            textColor = Color.RED;
+        }
+        else if (value == 2) {
+            text = "O";
+            textColor = Color.BLUE;
+        }
+        setCellValueEx(x, y, cellColor, text, textColor);
+    }
+
+}
